9c6fc4e82f6cf6ed48abafc311c55b26c97996ce
pxh <peixinhua@unisound.com>
2016-07-05 11:07:07 +0800

    添加更新说明文件

---
+++ UniOpusTest/iOS/UniOpus/UniOpus/Config.h
@@ -0,0 +1,17 @@
+//
+//  Config.h
+//  UniOpus
+//
+//  Created by pxh on 16/7/5.
+//  Copyright © 2016年 裴新华. All rights reserved.
+//
+
+#ifndef Config_h
+#define Config_h
+
+
+//--------------------- 版本更新说明 ------------------------\\
+//v1.0
+//UniOpusSDK第一个版本实现
+
+#endif /* Config_h */
