5afa70d42f514b06e27827c2ce647430443c40bb
pxh <peixinhua@unisound.com>
2016-12-08 18:55:33 +0800

    更新主题：直接编译通过生成.a 更新内容：

--- newLibOpus/include/Opus/libOpus.h
+++ newLibOpus/include/Opus/libOpus.h
@@ -8,6 +8,41 @@
 
 #import <Foundation/Foundation.h>
 
+@protocol OpusDelegate <NSObject>
+
+/**
+ *  opus编码完成后编码数据回调
+ *
+ *  @param encodeData 编码数据
+ */
+-(void)opusDataDidEncode:(NSData *)encodeData;
+
+/**
+ *  编码完成回调
+ */
+-(void)opusDataDidFinished;
+
+@end
+
 @interface libOpus : NSObject
+
+@property (nonatomic,assign)id <OpusDelegate>delegate;
+
+/**
+ *  opus编码函数
+ *  按照buffer进行编码
+ *  @param data 传入需要编码的音频数据
+ */
+-(void)appendAudioData:(NSData *)data;
+
+/**
+ *  停止编码
+ */
+-(void)stopEncode;
+
+/**
+ *  退出编码
+ */
+-(void)cancelEncode;
 
 @end
