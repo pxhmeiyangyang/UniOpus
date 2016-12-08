d07b35d04f1244ceda3387f961bd87be7e638521
pxh <peixinhua@unisound.com>
2016-12-08 18:52:34 +0800

    更新主题：修改了.a的输出路径 更新内容：

---
+++ newLibOpus/include/Opus/libOpus.h
@@ -0,0 +1,13 @@
+//
+//  libOpus.h
+//  libOpus
+//
+//  Created by pxh on 2016/12/8.
+//  Copyright © 2016年 pxh. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface libOpus : NSObject
+
+@end
