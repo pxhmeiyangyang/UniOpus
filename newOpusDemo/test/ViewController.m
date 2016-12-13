476a84516a67883e011eb26d03710ed1db4cef82
pxh <peixinhua@unisound.com>
2016-12-13 15:21:08 +0800

    更新主题：还原一下opus的代码 更新内容：

--- newOpusDemo/test/ViewController.m
+++ newOpusDemo/test/ViewController.m
@@ -8,7 +8,7 @@
 
 #import "ViewController.h"
 
-@interface ViewController()<OpusDelegate>
+@interface ViewController()/*<OpusDelegate>*/
 @property(nonatomic,strong)libOpus* opus;
 @property(nonatomic,strong)NSMutableData* data;
 @property(nonatomic,assign)BOOL isEncode;
@@ -21,36 +21,36 @@
     // Do any additional setup after loading the view, typically from a nib.
     _data = [NSMutableData data];
     _opus = [[libOpus alloc]init];
-    _opus.delegate = self;
-    _isEncode = false;
-//    __weak __typeof(&*self)weakSelf = self;
-//    _opus.opusDataDidEncode = ^(NSData* encodeData){
-//        
-//        [weakSelf.data appendData:encodeData];
-//        NSLog(@"opusDataDidEncode:%ld",encodeData.length);
-//    };
-//    _opus.opusDataDidFinished = ^(){
-//        NSLog(@"======编码完成了  : %ld",weakSelf.data.length);
-//        NSString* cachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES).firstObject;
-//        NSString* path = @"";
-//        if (weakSelf.isEncode) {
-//            path = [NSString stringWithFormat:@"%@/cacheTest.opus",cachePath];
-//        }else{
-//            path = [NSString stringWithFormat:@"%@/cacheTest.pcm",cachePath];
-//        }
-//    
-//    //    NSString* pcmPath = [[NSBundle mainBundle] pathForResource:@"testtest" ofType:@"opus"];
-//        if([weakSelf.data writeToFile:path atomically:NO]){
-//            NSLog(@"写入成功");
-//        }
-//    };
+//    _opus.delegate = self;
+    _isEncode = true;
+    __weak __typeof(&*self)weakSelf = self;
+    _opus.opusDataDidEncode = ^(NSData* encodeData){
+        
+        [weakSelf.data appendData:encodeData];
+        NSLog(@"opusDataDidEncode:%ld",encodeData.length);
+    };
+    _opus.opusDataDidFinished = ^(){
+        NSLog(@"======编码完成了  : %ld",weakSelf.data.length);
+        NSString* cachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES).firstObject;
+        NSString* path = @"";
+        if (weakSelf.isEncode) {
+            path = [NSString stringWithFormat:@"%@/cacheTest.opus",cachePath];
+        }else{
+            path = [NSString stringWithFormat:@"%@/cacheTest.pcm",cachePath];
+        }
+    
+    //    NSString* pcmPath = [[NSBundle mainBundle] pathForResource:@"testtest" ofType:@"opus"];
+        if([weakSelf.data writeToFile:path atomically:NO]){
+            NSLog(@"写入成功");
+        }
+    };
     NSString* type = @"pcm";
     if (_isEncode) {
         type = @"pcm";
     }else{
         type = @"opus";
     }
-    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"148153716649859" ofType:type];
+    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"test" ofType:type];
 //    NSString* filePath = [[NSBundle mainBundle] pathForResource:@"cacheTest" ofType:type];
     NSData* data = [NSData dataWithContentsOfFile:filePath];
     NSLog(@"====%lu",(unsigned long)data.length);
@@ -58,30 +58,30 @@
 }
 
 
--(void)opusDataDidEncode:(NSData *)encodeData{
-    [_data appendData:encodeData];
-    NSLog(@"opusDataDidEncode:%ld",encodeData.length);
-}
-
-/**
- *  编码完成回调
- */
--(void)opusDataDidFinished{
-    NSLog(@"======编码完成了  : %ld",_data.length);
-    NSString* cachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES).firstObject;
-    NSString* path = @"";
-    if (_isEncode) {
-        path = [NSString stringWithFormat:@"%@/cacheTest.opus",cachePath];
-    }else{
-        path = [NSString stringWithFormat:@"%@/cacheTest.pcm",cachePath];
-    }
-   
-//    NSString* pcmPath = [[NSBundle mainBundle] pathForResource:@"testtest" ofType:@"opus"];
-    if([_data writeToFile:path atomically:NO]){
-        NSLog(@"写入成功");
-    }
-    
-}
+//-(void)opusDataDidEncode:(NSData *)encodeData{
+//    [_data appendData:encodeData];
+//    NSLog(@"opusDataDidEncode:%ld",encodeData.length);
+//}
+//
+///**
+// *  编码完成回调
+// */
+//-(void)opusDataDidFinished{
+//    NSLog(@"======编码完成了  : %ld",_data.length);
+//    NSString* cachePath = NSSearchPathForDirectoriesInDomains(NSDocumentDirectory,  NSUserDomainMask, YES).firstObject;
+//    NSString* path = @"";
+//    if (_isEncode) {
+//        path = [NSString stringWithFormat:@"%@/cacheTest.opus",cachePath];
+//    }else{
+//        path = [NSString stringWithFormat:@"%@/cacheTest.pcm",cachePath];
+//    }
+//   
+////    NSString* pcmPath = [[NSBundle mainBundle] pathForResource:@"testtest" ofType:@"opus"];
+//    if([_data writeToFile:path atomically:NO]){
+//        NSLog(@"写入成功");
+//    }
+//    
+//}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
